72622826bd0702d9475a69172759d5b47a36847d
David Dubins <d.dubins@utoronto.ca>
2026-02-06 23:35:16 -0500

    Add files via upload

--- R-project/Oral_1comp.R
+++ R-project/Oral_1comp.R
@@ -1,5 +1,7 @@
+#Oral_1comp.R
 #This script fits PK data following oral administration with 1-compartment elimination
-#to the appropriate model. Written by: David Dubins
+#Written by: David Dubins
+#Date: 05-Feb-26
 
 #Read in the data (Time, Concentration) for a Subject
 #For PC:
@@ -7,67 +9,75 @@
 #For MacOS:
 #PKexample <- read.table(pipe("pbpaste"), header=TRUE)
 
-attach(PKexample)
+#Save a bit of referencing work:
+#(otherwise we would need to write PKexample$Time and PKexample$Concentration everywhere)
+Time <- PKexample$Time 
+Concentration <- PKexample$Concentration
 
-#Plot the log-Concentration vs. Time plot:
-plot(Time,log(Concentration),main="Concentration vs. Time: Semi-log Plot", xlab="Time (hours)", ylab = "log(Concentration)")
+#Have a look at the data to make sure it was imported correctly:
+plot(Time,Concentration,main = "Concentration vs. Time", xlab = "Time (h)", ylab = "Concentration (µg/mL)")
 
-#An easy way to start is to define the model C(t,1) as a function called Ct1:
-
+#Define the model C(t,1) as a function called Ct1:
 Ct1 <- function(A,ka,ke) {
 A*(ka/(ke-ka))*(exp(-ka*Time)-exp(-ke*Time))
 }
 
-#Now, we never have to write the equation again in the program.
-
 #Define the chisq function as (Yobs-Ymodel)^2 (we are going to minimize this)
-
 chisq <- function(p) sum((Concentration - Ct1(p[1],p[2],p[3]))^2)
-
 #We are using "p" as an array of parameters:
 #p[1] = A
 #p[2] = ka
 #p[3] = ke
 
 #Now guess some values on a new plot.
-#Set up variable names g1 to g5 --> easier to change 1st guesses.
+#Set up variable names g1 to g3 --> easier to change 1st guesses.
 #(replace 1, 2, and 3 with your starting guesses)
 
-plot(Time,Concentration,main="Concentration vs. Time", xlab="Time (hours)", ylab = "Concentration (ng/mL)")
-g1 <- 25    #first guess for A
-g2 <- 1.5    #first guess for ka
-g3 <- 0.0488984    #first guess for ke
-PKfitx <- PKexample$Time
+plot(Time,Concentration,main = "Concentration vs. Time", xlab = "Time (h)", ylab = "Concentration (µg/mL)")
+g1 <- 1    #first guess for A
+g2 <- 1    #first guess for ka
+g3 <- 1    #first guess for ke
+PKfitx <- Time
 PKfity <- Ct1(g1,g2,g3)
-lines(spline(PKfitx, PKfity))
+lines(smooth.spline(PKfitx, PKfity, spar = 0)) #spar is smoothing param (0.0: no smoothing, 1.0:too far)
 
 #Do do the actual fit with the starting values we found. This is done
 #by minimizing the function we defined using the starting values we just tried:
 #(replace 1, 2, and 3 with your best guesses)
-
 fit1 <- nlm(chisq, p = c(g1, g2, g3), hessian = TRUE)
 
 #To see the results of the fitting routine:
 fit1
 
-#Now superimpose our minimized fit on a brand new plot:
-plot(Time,Concentration,main="Concentration vs. Time", xlab="Time (hours)", ylab = "Concentration (ng/mL)")
-PKfitx <- PKexample$Time
+#Now superimpose our minimized fit on a brand-new plot:
+plot(Time,Concentration,main="Concentration vs. Time", xlab="Time (h)", ylab = "Concentration (ng/mL)")
+PKfitx <- Time
 PKfity <- Ct1(fit1$estimate[1],fit1$estimate[2],fit1$estimate[3])
-lines(spline(PKfitx, PKfity))
+lines(smooth.spline(PKfitx, PKfity, spar=0))
 
-#Now superimpose our minimized fit on a brand new log plot:
-plot(Time,log(Concentration),main="ln(Concentration) vs. Time", xlab="Time (hours)", ylab = "ln(Concentration) (ng/mL)")
-PKfitx <- PKexample$Time
+#Now superimpose our minimized fit on a brand-new log plot:
+plot(Time,log(Concentration),main="ln(Concentration) vs. Time", xlab="Time (h)", ylab = "ln(Concentration) (ln(ng/mL))")
+PKfitx <- Time
 PKfitlny <- log(Ct1(fit1$estimate[1],fit1$estimate[2],fit1$estimate[3]))
-PKfitlny[1]=NA  #This is to avoid crashing when plotting log(0)
-lines(spline(PKfitx, PKfitlny))
+PKfitlny[1]=0  #This is to avoid crashing when plotting log(0)
+lines(smooth.spline(PKfitx, PKfitlny, spar = 0))
 
-#To obtain the approximate standard errors of the parameter estimates:
-stderr <- sqrt(diag(2*fit1$minimum/(length(Concentration) - 2) * solve(fit1$hessian)))
+#Now plot the residuals to see if they look reasonable:
+Resid <- PKexample$Concentration-Ct1(fit1$estimate[1],fit1$estimate[2],fit1$estimate[3])
+plot(Time,Resid,main="Residuals vs. Time", xlab="Time (h)", ylab = "Cobserved - Cfit")
+abline(h = 0)  #plot a line at y=0
+
+#Solve for the correlation matrix:
+cov.mat <- 2 * fit1$minimum / (length(Concentration) - 2) * solve(fit1$hessian)
+rownames(cov.mat) <- colnames(cov.mat) <- c("A","ka","ke")
+cor.mat <- cov2cor(cov.mat)
+cor.mat
+
+# Calculate the approximate standard errors of the parameter estimates:
+stderr <- sqrt(diag(cov.mat))
 stderr
 
-#Calculate the rsq:
+#Calculate the coefficient of determination (r-squared) for the model:
 sstot = sum((Concentration-mean(Concentration))^2)
 sserr = sum((Concentration - PKfity)^2)
 rsq = 1-sserr/sstot
@@ -78,3 +88,8 @@ log(2)/fit1$estimate[2]
 
 #elimination half-life: (hours)
 log(2)/fit1$estimate[3]
+
+#Plot the correlation matrix (nice colour plot)
+#Look for off-diagonal numbers > 0.8 or <-0.8 here. That's bad!
+library(corrplot)
+corrplot(cor.mat, method = "color", addCoef.col = "black", number.digits = 3)
--- R-project/Sigmoidfit_norm.R
+++ R-project/Sigmoidfit_norm.R
@@ -1,5 +1,5 @@
-#Sigmoidfit.R
-#This script fits a sigmoid model to data (X values, Y values)
+#Sigmoidfit_norm.R
+#This script fits a sigmoid model to normalized data (X values, Y values (0-1))
 #Written by: David Dubins
 #Date: 06-Feb-26
 
@@ -9,54 +9,51 @@
 #Mydata <- read.table(pipe("pbpaste"), header=TRUE)
 
 #Save a bit of referencing work:
-#(otherwise we would need to write Mydata$X and Mydata$Y everywhere)
+#(otherwise we would need to write Mydata$X and Mydata$Ynorm everywhere)
 X <- Mydata$X
-Y <- Mydata$Y
+Ynorm <- Mydata$Ynorm
 
 # Define the model you are using to fit the data (in this case, sigmoid):
-Sigfn <- function(A,B,C,D) {
-  (D+(A/(1+exp(-B*(X-C)))))
+Sigfn <- function(B,C) {
+  (1/(1+exp(-B*(X-C))))
 }
 
 # Define the chisq function as sum(Yobs - Ymodel)^2:
-chisq <- function(p) sum((Y-Sigfn(p[1],p[2],p[3],p[4]))^2)
+chisq <- function(p) sum((Ynorm-Sigfn(p[1],p[2]))^2)
 # Now plot your data:
-plot(X,Y)
+plot(X,Ynorm)
 
 # Try your hand at fitting your parameters to get in the right ballpark. 
-# Myfity has your first guesses at g1, g2, g3, and g4 (replace 1, 2, 3, 4
-# with your first guesses)
+# Myfity has your first guesses at g1, g2
 
-g1 <- 9.5 # first guess for parameter A
-g2 <- 0.5 # first guess for parameter B
-g3 <- 21 # first guess for parameter C
-g4 <- 3 # first guess for parameter D
+g1 <- 0.4 # first guess for parameter B
+g2 <- 21 # first guess for parameter C
 
 Myfitx <- X
-Myfity <- Sigfn(g1,g2,g3,g4)
+Myfity <- Sigfn(g1,g2)
 
 # Plot your guess on the plot:
 lines(smooth.spline(Myfitx, Myfity, spar = 0))   #spar is smoothing param (0.0: no smoothing, 1.0:too far)
 
 # Ready for fitting the model with your guesses:
-Myfitout <- nlm(chisq, p = c(g1, g2, g3, g4), hessian = TRUE)
+Myfitout <- nlm(chisq, p = c(g1, g2), hessian = TRUE)
 # See the results of your fit:
 Myfitout
 
 # Now superimpose our minimized fit on a brand new plot:
-plot(X,Y)
+plot(X,Ynorm)
 Myfitx <- Mydata$X
-Myfity <- Sigfn(Myfitout$estimate[1],Myfitout$estimate[2],Myfitout$estimate[3],Myfitout$estimate[4])
+Myfity <- Sigfn(Myfitout$estimate[1],Myfitout$estimate[2])
 lines(smooth.spline(Myfitx, Myfity, spar=0))
 
 #Now plot the residuals to see if they look reasonable:
-Resid <-Y-Sigfn(Myfitout$estimate[1],Myfitout$estimate[2],Myfitout$estimate[3],Myfitout$estimate[4])
+Resid <-Ynorm-Sigfn(Myfitout$estimate[1],Myfitout$estimate[2])
 plot(X,Resid,main="Residuals vs. Time", xlab="X", ylab = "Yobserved - Yfit")
 abline(h = 0)  #plot a line at y=0
 
 #Solve for the correlation matrix:
-cov.mat <- 2 * Myfitout$minimum / (length(Y) - 2) * solve(Myfitout$hessian)
-rownames(cov.mat) <- colnames(cov.mat) <- c("A","B","C","D")
+cov.mat <- 2 * Myfitout$minimum / (length(Ynorm) - 2) * solve(Myfitout$hessian)
+rownames(cov.mat) <- colnames(cov.mat) <- c("B","C")
 cor.mat <- cov2cor(cov.mat)
 cor.mat
 
@@ -65,14 +62,14 @@ stderr <- sqrt(diag(cov.mat))
 stderr
 
 #Calculate the coefficient of determination (r-squared) for the model:
-sstot = sum((Y-mean(Y))^2)
-sserr = sum((Y - Myfity)^2)
+sstot = sum((Ynorm-mean(Ynorm))^2)
+sserr = sum((Ynorm - Myfity)^2)
 rsq = 1-sserr/sstot
 rsq
 
 # Now calculate the midpoint of the transition, (MidX, MidY)
-MidX <- Myfitout$estimate[3]
-MidY <- (Myfitout$estimate[4]+(Myfitout$estimate[1]/(1+exp(-Myfitout$estimate[2]*(MidX-Myfitout$estimate[3])))))
+MidX <- Myfitout$estimate[2]
+MidY <- (1/(1+exp(-Myfitout$estimate[1]*(MidX-Myfitout$estimate[2]))))
 
 #Report the midpoint of the transition on the X-axis and the value of Y at this point:
 MidX
